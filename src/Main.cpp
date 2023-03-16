60405fa778d109ce92386384078dc3f620611b26
Hippolyte DAVID <hippolyte.david@epitech.eu>
2023-03-16 10:04:32 +0100

    [MODIFY] :sparkles: --> Old main

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,20 +6,10 @@
 */
 
 #include "Parser.hpp"
-#include "DLLoader.hpp"
-#include "IGraphic.hpp"
 
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
-    // if (checkArgs(ac, av) == 84)
-    // return 84;
-    DLLoader<IGraphic> testLoader;
-    std::unique_ptr<IGraphic> graphLib = testLoader.getInstance("./lib/arcade_sfml.so");
-    graphLib->createWindow( "Allo ca marche ?", 400, 400);
-    while (1) {
-        graphLib->displayWindow();
-    }
+    if (checkArgs(ac, av) == 84)
+        return 84;
     return 0;
 }
